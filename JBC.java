398b8e20fe10f557ffedb754685df9367319a894
samuelc102 <samuel.colombo@barstowschool.org>
2018-08-28 09:46:07 -0500

    Add files via upload

---
+++ JBC.java
@@ -0,0 +1,26 @@
+package MagpieActivityStarterCode.activity3;
+
+
+/**
+ * Write a description of class JBC here.
+ *
+ * @author (Samuel Colombo)
+ * @version (8*28*2018)
+ */
+public class JBC
+{
+    private double stockPrice;
+    private double targetPrice;
+    /**
+     * Constructor for objects of class JBC
+     */
+    public JBC(double targetPrice)
+    {
+        this.targetPrice = targetPrice;
+    }
+
+    public double priceChange()
+    {
+        return something;
+    }
+}
